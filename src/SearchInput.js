d0af12602d9d0bad51f9e563d7ccade500ec674e
lighthouse-dev <lighthouse2.dev@gmail.com>
2021-07-10 01:03:58 +0900

    검색 페이지 관련 - 페이지 진입 시 포커스가 input 에 가도록 처리 - 키워드를 입력한 상태에서 input 을 클릭할 시에는 기존에 입력되어 있던 키워드 삭제

--- src/SearchInput.js
+++ src/SearchInput.js
@@ -15,6 +15,13 @@ class SearchInput {
       }
     });
 
+    // 페이지 진입 시 포커스가 input 에 가도록 처리하고,
+    // 키워드를 입력한 상태에서 input 을 클릭할 시에는 기존에 입력되어 있던 키워드가 삭제되도록 만들어야 합니다.
+    $searchInput.focus();
+    $searchInput.addEventListener('focusin', e => {
+      $searchInput.value = '';
+    });
+
     console.log('SearchInput created.', this);
   }
   render() {}
